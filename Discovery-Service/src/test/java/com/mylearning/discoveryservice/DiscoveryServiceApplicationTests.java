42556f9f55ae0365f67183e91807852118fa85fa
prashant <prashanttiwary10@gmail.com>
2025-05-29 15:43:41 +0530

    service discovery and registry

---
+++ Discovery-Service/src/test/java/com/mylearning/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylearning.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
